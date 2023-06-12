494f6396eaa445f6f14cb96dadc68b8402514bfe
Jdavid <201705961@est.umss.edu>
2023-06-12 12:10:09 -0400

    create styled React pages and establish backend integration

--- a2o_test/a2o_test/settings.py
+++ a2o_test/a2o_test/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'problems',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'a2o_test.urls'
@@ -123,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:3000',
+#     'http://example.com',
+#     ...
+# ]
--- a2o_test/problems/views.py
+++ a2o_test/problems/views.py
@@ -1,19 +1,32 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-@api_view(['GET'])
+from problems.solution.Solution2 import Solution2
+from rest_framework import status
+from problems.solution.Solution1 import  queensAttack
+@api_view(['POST'])
 def problem1(request):
     # Controller logic for problem1
-    data = {
-        'message': 'This is problem1.'
-    }
-    return Response(data)
+    try: 
+          rq=int(request.data.get('rq'))-1
+          data = {
+            'message': queensAttack(int(request.data.get('n')),int(request.data.get('k')),rq,int(request.data.get('cq'))-1,request.data.get('obstacles')),
+             'ok':True
+          }
+          return Response(data)
+    except ValueError as e:    
+            return Response(f"An error occurred: {str(e)}", status=status.HTTP_500_INTERNAL_SERVER_ERROR)      
+   
 
-@api_view(['GET'])
+@api_view(['POST'])
 def problem2(request):
     # Controller logic for problem2
-    data = {
-        'message': 'This is problem2.'
-    }
-    return Response(data)
+    try:
+            solution=Solution2(request.data.get('input'))
+            data = {
+                'response': solution.calculate_max_value(),
+                 'ok':True,
+            }
+            return Response(data, status=status.HTTP_200_OK)
+    except ValueError as e:    
+            return Response(f"An error occurred: {str(e)}", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
